90bfe32ac3ed4bfe46a2274a7de9a3d8d755869b
Gabriel Souza Santos <gabrielsouzadjsantos@gmail.com>
2022-11-02 13:58:39 -0300

    Update MetodosComRetorno.cs

--- CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
+++ CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
@@ -18,11 +18,45 @@ namespace CursoCSharp.ClassesEMetodos
         {
             return a * b;
         }
-        public int Dividir(int a, int b)
+        /*public int Dividir(int a, int b)
         {
             return a / b;
+        }*/
+    }
+
+    class CalculadoraCadeia
+    {
+        int memoria;
+
+        public CalculadoraCadeia Somar(int a)
+        {
+            memoria += a;
+            return this;
+        }
+
+        public CalculadoraCadeia Multiplicar(int a)
+        {
+            memoria *= a;
+            return this;
+        }
+        public CalculadoraCadeia Limpar()
+        {
+            memoria = 0;
+            return this;
+        }
+
+        public CalculadoraCadeia Imprimir()
+        {
+            Console.WriteLine(memoria);
+            return this;
+        }
+
+        public int Resultado()
+        {
+            return memoria;
         }
     }
+
     class MetodosComRetorno
     {
         public static void Executar()
@@ -32,7 +66,13 @@ namespace CursoCSharp.ClassesEMetodos
             Console.WriteLine(resultado);
             Console.WriteLine(calculadoraComum.Subtrair(2, 7));
             Console.WriteLine(calculadoraComum.Multiplicar(4, 4));
-            Console.WriteLine(calculadoraComum.Dividir(7, 7));
+            //Console.WriteLine(calculadoraComum.Dividir(7, 7));
+
+            var calculadoraCadeia = new CalculadoraCadeia();
+            calculadoraCadeia.Somar(3).Multiplicar(3).Imprimir().Limpar().Imprimir();
+
+            resultado = calculadoraCadeia.Somar(3).Multiplicar(2).Resultado();
+            Console.WriteLine(resultado);
         }
     }
 }
