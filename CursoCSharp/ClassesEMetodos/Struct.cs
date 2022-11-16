95819fae70f641c14296415772e94f98380f15af
Gabriel Souza Santos <gabrielsouzadjsantos@gmail.com>
2022-11-16 16:38:51 -0300

    Create Struct.cs

---
+++ CursoCSharp/ClassesEMetodos/Struct.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    interface Ponto
+    {
+        void MoverNaDiagonal(int delta); // Assim por padrão é publica!!!
+    }
+
+    struct Coordenada: Ponto
+    {
+        public int X;
+        public int Y;
+
+        public Coordenada(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void MoverNaDiagonal(int delta)
+        {
+            X += delta; // atribuição aditiva
+            Y += delta; // atribuição aditiva
+        }
+    }
+    class ExemploStruct
+    {
+        public static void Executar()
+        {
+            Coordenada coordenadaInicial;
+            coordenadaInicial.X = 2;
+            coordenadaInicial.Y = 2;
+
+            Console.WriteLine("Coordenada Inicial:");
+            Console.WriteLine("X = {0}", coordenadaInicial.X);
+            Console.WriteLine("Y = {0}", coordenadaInicial.Y);
+
+            var coordenadaFinal = new Coordenada(x: 9, y: 1);
+            coordenadaFinal.MoverNaDiagonal(10);
+
+            Console.WriteLine("Coordenada Final:");
+            Console.WriteLine("X = {0}", coordenadaFinal.X);
+            Console.WriteLine("Y = {0}", coordenadaFinal.Y);
+
+        }
+    }
+}
